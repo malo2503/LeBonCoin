7dc2edd221fc96a1471b3cebe18ab7e1de39cf95
Sylvia ROMEO <malo2503@users.noreply.github.com>
2018-02-06 16:53:02 +0100

    create database mongodb

--- index.js
+++ index.js
@@ -1,58 +1,76 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 var _ = require("lodash");
 
-var multer = require('multer');
-var upload = multer({ dest: 'public/uploads/' });
+var multer = require("multer");
+var upload = multer({ dest: "public/uploads/" });
+
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/leboncoin");
+
+// 1) Definir le schema - A faire qu'une fois
+var offerSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: String,
+  picture: String,
+  location: String,
+  pseudo: String,
+  email: String,
+  phone: String,
+});
+
+// 2) Definir le model - A faire qu'une fois
+var ClassifiedAd = mongoose.model("ClassifiedAd", offerSchema);
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var offers =[];
 
-app.get('/', function (req, res) {
-    res.render('home.ejs');
-  });
+app.get("/", function(req, res) {
+  res.render("home.ejs");
+});
 
 // accéder au formulaire de depot
 
-app.get('/deposer', function (req,res) {
-    res.render('placeoffer.ejs');
+app.get("/deposer", function(req, res) {
+  res.render("placeoffer.ejs");
 });
-
 
 //creer son annonce
 
-app.post ("/deposer", upload.single("picture"), function(req,res){
-  var NewOffer = {};
-  NewOffer.id = offers.length + 1;
-  NewOffer.title = req.body.title;
-  NewOffer.description = req.body.description;
-  NewOffer.price = req.body.price;
-  NewOffer.picture = req.file.filename;
-  NewOffer.location = req.body.location;
-  NewOffer.pseudo = req.body.pseudo;
-  NewOffer.email = req.body.email;
-  NewOffer.phone = req.body.phone;
-      
-
-  offers.push(NewOffer);
-  /* console.log(offers); */
-  res.redirect('/annonce/'+ NewOffer.id);
+app.post("/deposer", upload.single("picture"), function(req, res) {
+  var NewOffer = new ClassifiedAd({
+  title : req.body.title,
+  description : req.body.description,
+  price : req.body.price,
+  picture : req.file.filename,
+  location : req.body.location,
+  pseudo : req.body.pseudo,
+  email : req.body.email,
+  phone : req.body.phone,
+  });
+  NewOffer.save(function(err, obj) {
+    if (err) {
+      console.log("something went wrong");
+    } else {
+      console.log("we just saved the new Offer " + obj.title);
+    }
+  res.redirect("/annonce/" + NewOffer.id);
+  });
 });
 
 //accéder au rendu d'annonce
 
-app.get('/annonce/:id', function (req, res) {
+/*app.get("/annonce/:id", function(req, res) {
   var id = parseInt(req.params.id);
-  var offer = _.filter(offers, ["id", id]);
+  var offer = _.find(offers, ["id", id]);
   console.log(offer);
-  res.render('viewoffer.ejs', {offer});
+  res.render("viewoffer.ejs", { offer });
 });
-
+*/
 
 app.listen(3000, "localhost", function() {
-console.log("Server is listening...");
+  console.log("Server is listening...");
 });
-  
